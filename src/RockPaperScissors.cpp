93990256d0b82fd61ba758c68d47f6befc762a04
Joseph Gebretsadik <josephgebretsadik@eaton.com>
2024-06-27 13:06:52 -0700

    all tests passing v3

--- src/RockPaperScissors.cpp
+++ src/RockPaperScissors.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 string RockPaperScissorsPlay(const string player1, const string player2)
 {
-    if ((player1 == "Scissors" && player2 == "Paper") || (player1 == "Paper" && player2 == "Rock") || (player1 == "Rock" && player2 == "Scissors"))
+    if ((player1 == "Scissors" && player2 == "Paper") || (player1 == "Paper" && player2 == "Rock") 
+    || (player1 == "Rock" && player2 == "Scissors"))
     {
         return "Player1 Wins";
     }
